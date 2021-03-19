92238124abe091a42290cd1d85899e61dea00215
sulekooc <sulekooc@gmail.com>
2021-03-19 22:09:01 +0300

    ders10 kodları eklendi

---
+++ ders10/main.c
@@ -0,0 +1,50 @@
+/*
+ Pointer Pointers Example
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//[p]->[p1]->a
+
+void Test( void **p ) {
+
+    *p = malloc(1200);
+}
+
+
+int main()
+{
+/*
+    char a = 65;
+    char   *p1 = &a;
+    char **p = &p1;
+    char ***p = &p;
+*/
+
+
+
+
+    char *tp = NULL;
+    Test( &tp );
+    if( tp != NULL )
+       free( tp );
+    else
+       printf( "Hata\n" );
+
+
+
+    //*p1 = 5;
+
+    //**p = 8;
+
+    //char szBuffer[12];
+
+
+    //szBuffer[11] = NULL;
+    printf("Hello world!\n");
+    return 0;
+
+}
